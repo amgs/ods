9b35460f9265b0959741e52d8e01535562a100f9
André Sabino <705800+amgs@users.noreply.github.com>
2023-06-05 15:54:54 +0100

    Set default update interval.

--- sensor-server/app.py
+++ sensor-server/app.py
@@ -5,7 +5,7 @@ import requests
 COM_PORT = "COM3"
 BAUD_RATES = 9600
 SERVER_URL = "https://narrativelab.org/cgi-bin/workspace-monitor.py"
-UPDATE_INTERVAL = 50 # seconds
+UPDATE_INTERVAL = 60 # seconds
 DEBUG = False
 
 def main():
